77666dbb2cd73805fbc50e8b8e94259414299f93
nixfu <redditbot@galena.dreamhost.com>
2019-02-26 17:18:44 -0800

    Reworked startup script used by cronjob

--- startbot.sh
+++ startbot.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+export EDITOR=vi
+export PIP_USER=yes
+export PATH=/home/redditbot/.local/bin:$PATH
+alias startlean="cd ~/github/bots/userleansbot;/usr/bin/screen -dmS userleansbot python3 bot.py"
+alias leanlog="tail -f ~/github/bots/userleansbot/bot.log"
+alias leanstatus="ps -ef|grep bot.py |grep -v grep"
 
 BOTDIR="/home/redditbot/github/bots/userleansbot"
 cd $BOTDIR
 
+
 if ! ps -ef |grep -v grep | grep -q "python3 bot.py"; then
-	ls -l 
+	#tmux new-session -d -s userleansbot 'python3 bot.py'
     	/usr/bin/screen -dmS userleansbot python3 bot.py
 else
-	#echo "Bot running"
+	#echo "Bot running" >> ${BOTDIR}/cron.log
 	exit 0
 fi
 
